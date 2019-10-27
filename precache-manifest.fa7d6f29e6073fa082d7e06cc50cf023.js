c9a2bd2438a8f4a1abb48506fe3f134d458b23c7
Idan Felz <idanf2@gmail.com>
2019-10-27 15:24:10 +0200

    Updates

---
+++ precache-manifest.fa7d6f29e6073fa082d7e06cc50cf023.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401f74eaf50d145bd013f93c091f8ca3",
+    "url": "/idanf2/react-monsters/index.html"
+  },
+  {
+    "revision": "a22a56d6c4aac6c3c583",
+    "url": "/idanf2/react-monsters/static/css/main.f9f3602f.chunk.css"
+  },
+  {
+    "revision": "82128c5ae226b744f7d0",
+    "url": "/idanf2/react-monsters/static/js/2.75d5cf1d.chunk.js"
+  },
+  {
+    "revision": "a22a56d6c4aac6c3c583",
+    "url": "/idanf2/react-monsters/static/js/main.58375acd.chunk.js"
+  },
+  {
+    "revision": "a2436c99f60907e9eccb",
+    "url": "/idanf2/react-monsters/static/js/runtime-main.bfc4678e.js"
+  }
+]);
